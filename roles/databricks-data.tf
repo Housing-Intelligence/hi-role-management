c067b5153980325f429be267a6a02611681f3a1c
Alvis <alviswhyi@gmail.com>
2026-09-13 17:36:40 +0800

    Add self assuming

--- roles/databricks-data.tf
+++ roles/databricks-data.tf
@@ -41,7 +41,9 @@ resource "aws_iam_role" "databricks_data" {
       {
         Effect = "Allow"
         Principal = {
-          AWS = "arn:aws:iam::414351767826:role/unity-catalog-prod-UCMasterRole-14S5ZJVKOTYTL"
+          AWS = 【
+            "arn:aws:iam::414351767826:role/unity-catalog-prod-UCMasterRole-14S5ZJVKOTYTL",
+            "arn:aws:iam::844096318338:role/databricks-housing-s3-role"
         }
         Action = "sts:AssumeRole"
         Condition = {
